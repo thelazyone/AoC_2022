66708cf40e092f6c4aa071bcd61948307f13e443
thelazyone <giacomo.pantalone@gmail.com>
2022-12-27 21:39:00 +0000

    Finished with day 11

--- exercise_11/src/main.rs
+++ exercise_11/src/main.rs
@@ -1,8 +1,13 @@
 // Exercise 9: calculating the path of a cursor following the movement of another
+// Implementing only part 2, because the optimization made part 1 unfeasible : the "/3"
+// operation doesn't work well with the implemented structure.
+
 
 // For reading/parsing
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+
+use std::collections::HashMap;
 
 // parsing the info about the monkey
 struct Monkey {
@@ -12,105 +17,52 @@ struct Monkey {
     inspect_counter : i32,
 }
 
-// Instead of a counter i'm using a rotating index for all the prime numbers. 
+
+// Using a rotating index for all the prime numbers. 
 #[derive(Clone)]
+#[derive(Debug)]
 struct RotatingCounter {
-    counter_2 : i32,
-    counter_3 : i32,
-    counter_5 : i32,
-    counter_7 : i32,
-    counter_11 : i32,
-    counter_13 : i32,
-    counter_17 : i32,
-    counter_19 : i32,
-    counter_23 : i32,
+    counters : HashMap<i32, i32>,
 }
 impl RotatingCounter {
 
     fn new(input : i32) -> RotatingCounter {
         RotatingCounter {
-            counter_2  : input % 2,
-            counter_3  : input % 3,
-            counter_5  : input % 5,
-            counter_7  : input % 7,
-            counter_11 : input % 11,
-            counter_13 : input % 13,
-            counter_17 : input % 17,
-            counter_19 : input % 19,
-            counter_23 : input % 23
-            }
+            counters: HashMap::from([
+                (2 , input % 2 ),
+                (3 , input % 3 ),
+                (5 , input % 5 ),
+                (7 , input % 7 ),
+                (11, input % 11),
+                (13, input % 13),
+                (17, input % 17),
+                (19, input % 19),
+                (23, input % 23)
+            ])
+        }
     }
 
     fn add(&self, input: i32) -> RotatingCounter {
-        RotatingCounter{
-            counter_2  : (self.counter_2  + input) % 2,
-            counter_3  : (self.counter_3  + input) % 3,
-            counter_5  : (self.counter_5  + input) % 5,
-            counter_7  : (self.counter_7  + input) % 7,
-            counter_11 : (self.counter_11 + input) % 11,
-            counter_13 : (self.counter_13 + input) % 13,
-            counter_17 : (self.counter_17 + input) % 17,
-            counter_19 : (self.counter_19 + input) % 19,
-            counter_23 : (self.counter_23 + input) % 23,
-            }
+        RotatingCounter{counters : self.counters.iter().map(move |(base, value)| {(*base, (value + input) % * base as i32)}).collect()}
     }
 
     // Multiplying means that residuals are multiplied with the input
     fn multiply(&self, input: i32) -> RotatingCounter {
-        RotatingCounter{
-            counter_2  : (self.counter_2  * input) % 2,
-            counter_3  : (self.counter_3  * input) % 3,
-            counter_5  : (self.counter_5  * input) % 5,
-            counter_7  : (self.counter_7  * input) % 7,
-            counter_11 : (self.counter_11 * input) % 11,
-            counter_13 : (self.counter_13 * input) % 13,
-            counter_17 : (self.counter_17 * input) % 17,
-            counter_19 : (self.counter_19 * input) % 19,
-            counter_23 : (self.counter_23 * input) % 23,
-            }
+        RotatingCounter{counters : self.counters.iter().map(move |(base, value)| {(*base, (value * input) % * base as i32)}).collect()}
     }
 
     // Squaring the value doesn't affect anything that is already multiple, but 
     // any residual is squared too.
     fn square(&self) -> RotatingCounter {
-        RotatingCounter{
-            counter_2  : (self.counter_2  * self.counter_2 ) % 2,
-            counter_3  : (self.counter_3  * self.counter_3 ) % 3,
-            counter_5  : (self.counter_5  * self.counter_5 ) % 5,
-            counter_7  : (self.counter_7  * self.counter_7 ) % 7,
-            counter_11 : (self.counter_11 * self.counter_11) % 11,
-            counter_13 : (self.counter_13 * self.counter_13) % 13,
-            counter_17 : (self.counter_17 * self.counter_17) % 17,
-            counter_19 : (self.counter_19 * self.counter_19) % 19,
-            counter_23 : (self.counter_23 * self.counter_23) % 23,
-            }
+        RotatingCounter{counters : self.counters.iter().map(move |(base, value)| {(*base, (value * value) % * base as i32)}).collect()}
     }
+
 
     fn is_divisible_by_prime(&self, input: i32) -> bool {
-        match input {
-            2  => return self.counter_2 == 0,
-            3  => return self.counter_3 == 0,
-            5  => return self.counter_5 == 0,
-            7  => return self.counter_7 == 0,
-            11 => return self.counter_11 == 0,
-            13 => return self.counter_13 == 0,
-            17 => return self.counter_17 == 0,
-            19 => return self.counter_19 == 0,
-            23 => return self.counter_23 == 0,
-            number => panic!("I'm expecting prime numbers below 24, got {}", number),
-        }
+        *self.counters.get(&input).unwrap() == 0
     }
 }
 
-enum OperatorType {
-    Mult, 
-    Sum,
-}
-
-enum SecondMember {
-    Old, 
-    IntValue(i32),
-}
 
 fn cleanup_monkey_input (mut input_lines : Vec<String>) -> Vec<String> {
     // Cleaning up the info
@@ -125,6 +77,8 @@ fn cleanup_monkey_input (mut input_lines : Vec<String>) -> Vec<String> {
     input_lines
 }
 
+
+// Reading the text input and parsing
 fn parse_monkey(input_lines : Vec<String>) -> Monkey {
 
     // Converting into iter, reading line after line and consuming it
@@ -144,8 +98,8 @@ fn parse_monkey(input_lines : Vec<String>) -> Monkey {
     let operator : Box<dyn Fn(&RotatingCounter, &i32)->RotatingCounter>;
     let operation : Box<dyn Fn(&RotatingCounter)->RotatingCounter>;
     match operation_elements[3].as_str() {
-        "*" => operator = Box::new(|elem_a : &RotatingCounter, elem_b : &i32| -> RotatingCounter {elem_a.add(elem_b.clone())}),
-        "+" => operator = Box::new(|elem_a : &RotatingCounter, elem_b : &i32| -> RotatingCounter {elem_a.multiply(elem_b.clone())}),
+        "*" => operator = Box::new(|elem_a : &RotatingCounter, elem_b : &i32| -> RotatingCounter {elem_a.multiply(elem_b.clone())}),
+        "+" => operator = Box::new(|elem_a : &RotatingCounter, elem_b : &i32| -> RotatingCounter {elem_a.add(elem_b.clone())}),
         _ => panic!("unexpected command!")
     }
 
@@ -170,8 +124,11 @@ fn parse_monkey(input_lines : Vec<String>) -> Monkey {
     let throwing_rule : Box<dyn Fn(&RotatingCounter)->i32>;
     throwing_rule = Box::new(move |value : &RotatingCounter| -> i32 {
         if value.is_divisible_by_prime(division_factor) {
+            if division_factor == 19 {
+            }
             return target_case_true;
         }
+        //println!("elem is not divisible by {}", division_factor);
         target_case_false
     });
 
@@ -230,6 +187,7 @@ fn execute (input_path : String)  -> Option<(u64, u64)> {
     }
 
     // Part 1 is not working anymore, since the optimization for part 2 doesn't work with divisions.
+    
     // // Each monkey then operates, 20 times:
     // for _ in 0..20 {
 
@@ -269,7 +227,7 @@ fn execute (input_path : String)  -> Option<(u64, u64)> {
     // For part 2 it's all the same, only without the /3 and iterating 10.000 times.
     // This however poses a problem - numbers become huge, so i should track the prime
     // numbers involved!
-    for _ in 0..1000 {
+    for _ in 0..10000 {
         // The right way to iterate here would be with the "for in" syntax, but 
         // since i'm modifying the vector inside the cycle i'll be using the index instead.
         for idx in 0..monkeys_vec.len() {
@@ -278,9 +236,7 @@ fn execute (input_path : String)  -> Option<(u64, u64)> {
             monkeys_vec[idx].inspect_counter += monkeys_vec[idx].items_vec.len() as i32;
             for item in monkeys_vec[idx].items_vec.clone() {
 
-                // Applies the operation and divide by 3.
-                // Since we're using a weird counter instead of a normal i32, dividing by a number
-                // is surprisingly complicated, so I implemented a "get value" method. 
+                // Applies the operation.
                 let new_item_value = (monkeys_vec[idx].operation)(&item);
 
                 // depending on the value throws the item to another monkey.
@@ -311,8 +267,8 @@ fn execute (input_path : String)  -> Option<(u64, u64)> {
 fn main() -> io::Result<()> {
     println!("Welcome to Advent of Code 2022 - Day 9!");
 
-    //let results = execute("./data/input.txt".to_string()).unwrap();
-    let results = execute("./data/test.txt".to_string()).unwrap();
+    let results = execute("./data/input.txt".to_string()).unwrap();
+    // let results = execute("./data/test.txt".to_string()).unwrap();
     
     println!("\nPart 1 result is {}.", results.0);
     println!("\nPart 2 result is {}.", results.1);
@@ -333,7 +289,85 @@ mod tests {
     // }    
 
     #[test]
-    fn global_test_part_2() {
-        assert_eq!(execute("./data/test.txt".to_string()).unwrap().1, 2713310158);
-    }    
+    fn unit_test_rotating_counter_divisible_by() {
+        let counter = RotatingCounter::new(65168454);
+        assert_eq!(counter.is_divisible_by_prime(2), true);
+        assert_eq!(counter.is_divisible_by_prime(3), true);
+        assert_eq!(counter.is_divisible_by_prime(5), false);
+        assert_eq!(counter.is_divisible_by_prime(7), false);
+        assert_eq!(counter.is_divisible_by_prime(11), false);
+        assert_eq!(counter.is_divisible_by_prime(13), true);
+        assert_eq!(counter.is_divisible_by_prime(17), false);
+        assert_eq!(counter.is_divisible_by_prime(19), false);
+        assert_eq!(counter.is_divisible_by_prime(23), false);
+    }
+
+    #[test]
+    fn unit_test_rotating_counter_add() {
+        let mut counter = RotatingCounter::new(350);
+        assert_eq!(counter.is_divisible_by_prime(2), true);
+        assert_eq!(counter.is_divisible_by_prime(3), false);
+        assert_eq!(counter.is_divisible_by_prime(5), true);
+        assert_eq!(counter.is_divisible_by_prime(7), true);
+        assert_eq!(counter.is_divisible_by_prime(11), false);
+        assert_eq!(counter.is_divisible_by_prime(13), false);
+        assert_eq!(counter.is_divisible_by_prime(17), false);
+        assert_eq!(counter.is_divisible_by_prime(19), false);
+        assert_eq!(counter.is_divisible_by_prime(23), false);
+        counter = counter.add(35);
+        assert_eq!(counter.is_divisible_by_prime(2), false);
+        assert_eq!(counter.is_divisible_by_prime(3), false);
+        assert_eq!(counter.is_divisible_by_prime(5), true);
+        assert_eq!(counter.is_divisible_by_prime(7), true);
+        assert_eq!(counter.is_divisible_by_prime(11), true);
+        assert_eq!(counter.is_divisible_by_prime(13), false);
+        assert_eq!(counter.is_divisible_by_prime(17), false);
+        assert_eq!(counter.is_divisible_by_prime(19), false);
+        assert_eq!(counter.is_divisible_by_prime(23), false);
+    }
+
+    #[test]
+    fn unit_test_rotating_counter_square() {
+        let mut counter = RotatingCounter::new(3234);
+        counter = counter.square(); //10458756
+        assert_eq!(counter.is_divisible_by_prime(2), true);
+        assert_eq!(counter.is_divisible_by_prime(3), true);
+        assert_eq!(counter.is_divisible_by_prime(5), false);
+        assert_eq!(counter.is_divisible_by_prime(7), true);
+        assert_eq!(counter.is_divisible_by_prime(11), true);
+        assert_eq!(counter.is_divisible_by_prime(13), false);
+        assert_eq!(counter.is_divisible_by_prime(17), false);
+        assert_eq!(counter.is_divisible_by_prime(19), false);
+        assert_eq!(counter.is_divisible_by_prime(23), false);
+        counter = counter.add(35);
+        assert_eq!(counter.is_divisible_by_prime(2), false);
+        assert_eq!(counter.is_divisible_by_prime(3), false);
+        assert_eq!(counter.is_divisible_by_prime(5), false);
+        assert_eq!(counter.is_divisible_by_prime(7), true);
+        assert_eq!(counter.is_divisible_by_prime(11), false);
+        assert_eq!(counter.is_divisible_by_prime(13), false);
+        assert_eq!(counter.is_divisible_by_prime(17), true);
+        assert_eq!(counter.is_divisible_by_prime(19), false);
+        assert_eq!(counter.is_divisible_by_prime(23), false);
+    }
+
+    #[test]
+    fn unit_test_rotating_counter_multiply() {
+        let mut counter = RotatingCounter::new(3234);
+        counter = counter.multiply(947); //3062598
+        assert_eq!(counter.is_divisible_by_prime(2), true);
+        assert_eq!(counter.is_divisible_by_prime(3), true);
+        assert_eq!(counter.is_divisible_by_prime(5), false);
+        assert_eq!(counter.is_divisible_by_prime(7), true);
+        assert_eq!(counter.is_divisible_by_prime(11), true);
+        assert_eq!(counter.is_divisible_by_prime(13), false);
+        assert_eq!(counter.is_divisible_by_prime(17), false);
+        assert_eq!(counter.is_divisible_by_prime(19), false);
+        assert_eq!(counter.is_divisible_by_prime(23), false);
+    }
+
+     #[test]
+     fn global_test_part_2() {
+         assert_eq!(execute("./data/test.txt".to_string()).unwrap().1, 2713310158);
+     }    
 }
