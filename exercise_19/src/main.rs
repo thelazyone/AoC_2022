57f224c82d676f7a87d582f40ea6f2369ea2f7ba
thelazyone <giacomo.pantalone@gmail.com>
2023-02-01 18:03:54 +0000

    Started Day 19, Regex has issues

--- exercise_19/src/main.rs
+++ exercise_19/src/main.rs
@@ -3,8 +3,150 @@
 // For reading/parsing
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+use regex::Regex;
+use std::collections::HashMap;
 
-// utility
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+enum ResourceType {
+    Ore = 0,
+    Clay = 1,
+    Obsidian = 2,
+    Geode = 3,
+    ResourcesNumber = 4,
+}
+type ResourcesVect = [u32; ResourceType::ResourcesNumber as usize];
+
+struct Blueprint {
+    robot_costs : HashMap<u32, ResourcesVect>,
+}
+impl Blueprint {
+    fn new_from_line(line : &String) -> Blueprint {
+        let regex_string = 
+        r"ore robot costs\s(?P<val1>\S+)\D+clay robot costs(?P<val2>\S+)";
+        let regex = Regex::new(regex_string).unwrap();
+        match regex.captures(line) {
+            Some(caps) => {
+                let val1 = caps.name("val1").unwrap().as_str().parse::<u32>().unwrap();
+                let val2 = caps.name("val2").unwrap().as_str().parse::<u32>().unwrap();
+                
+                Blueprint {
+                    robot_costs : [
+                        (ResourceType::Ore as u32, vec!{val1, 0, 0, 0}.try_into().unwrap()),
+                        (ResourceType::Clay as u32, vec!{val2, 0, 0, 0}.try_into().unwrap()),
+                        (ResourceType::Obsidian as u32, vec!{0, 0, 0, 0}.try_into().unwrap()),
+                        (ResourceType::Geode as u32, vec!{0, 0, 0, 0}.try_into().unwrap())].iter().cloned().collect()
+                }
+            }
+            None => panic!("Error reading line: {}b", line),
+        }
+    }
+
+    fn calculate_maximum_geode_yield(&self, max_steps : u32) -> u32 {
+
+        // Starting with a set of current robots (1 ore, none of the rest)
+        // and passing it in a recursive function.
+        
+        // Calling command "none" the first iteration, which means that no clay robot will be built
+        // This is ok with the input data, but it is a limit of the implementation
+        self.iterative_step_factory(max_steps, &vec!{1, 0, 0, 0}.try_into().unwrap(), &vec!{0, 0, 0, 0}.try_into().unwrap(), None)
+    }
+
+    fn iterative_step_factory (
+        &self, 
+        remaining_steps : u32, 
+        current_robots : &ResourcesVect, 
+        current_resources : &ResourcesVect,
+        command_to_build : Option<ResourceType>) -> u32 {
+
+        let mut new_resources = current_resources.clone();
+        let mut new_robots = current_robots.clone();
+        
+        // Checking the command, executing.
+        if let Some(resource_type) = command_to_build {
+            if Blueprint::resources_enough_for(&new_resources, &self.robot_costs[&(resource_type as u32)]) {
+
+                // Consuming the resources
+                new_resources = Blueprint::subtract_resources(&new_resources, &self.robot_costs[&(resource_type as u32)]);
+
+                // Adding a robot. But using the "current robots" value to calculate the extra resources later on.
+                new_robots[resource_type as usize] += 1
+            }
+            
+            // If there are no resources, closing this branch: it's an invalid one.
+            return 0;
+        }
+
+        // Adding the resources, one for each robot of that kind.
+        for idx in 0..ResourceType::ResourcesNumber as usize {
+            new_resources[idx] += current_robots[idx];
+        }
+
+        // If the max iteration has been reached, returning the value.
+        if remaining_steps == 0 {
+            return new_resources[ResourceType::Geode as usize]
+        }
+
+        // If we have more steps, sending all the commands for future branches.
+
+        // All final geodes results from the branching
+        let mut all_scores = Vec::<u32>::new();
+        for idx in 0..ResourceType::ResourcesNumber as usize {
+            all_scores.push(self.iterative_step_factory(
+                remaining_steps - 1,
+                &new_robots,
+                &new_resources,
+                Some(Blueprint::get_resource_type(idx as u32))));
+        }
+        
+        // Pushing the "do nothing" command too.
+        all_scores.push(self.iterative_step_factory(
+            remaining_steps - 1,
+            &new_robots,
+            &new_resources,
+            None));
+
+        // Returning the best of the results.
+        all_scores.iter().max().unwrap().clone()
+    }
+
+    // Utility functions
+
+    fn resources_enough_for (a : &ResourcesVect, b : &ResourcesVect) -> bool {
+        for idx in 0..ResourceType::ResourcesNumber as usize {
+            if a[idx] < b[idx] {
+                return false;
+            }
+        }
+        true
+    }
+    
+    fn subtract_resources (a : &ResourcesVect, b : &ResourcesVect) -> ResourcesVect {
+
+        // Sanity Check: 
+        if !Blueprint::resources_enough_for(a, b)
+        {
+            panic!("Invalid subtraction of {:?} from {:?}", b, a);
+        }
+
+        // Subtracting.
+        let mut result = a.clone();
+        for idx in 0..ResourceType::ResourcesNumber as usize{
+            result[idx] -= b[idx];
+        }
+        result
+    }
+
+    fn get_resource_type (number : u32) -> ResourceType {
+        match number {
+            0 => ResourceType::Clay,
+            1 => ResourceType::Clay,
+            2 => ResourceType::Clay,
+            3 => ResourceType::Clay,
+            _ => panic!("no resource with this value!")
+        }
+    }
+}
 
 // Primary Function
 fn execute (input_path : String)  -> Option<(u32, u32)> {
@@ -29,7 +171,16 @@ fn execute (input_path : String)  -> Option<(u32, u32)> {
     assert!(lines_vec.len() > 1);
 
     // Converting each line in a blueprint.
-    // TODO
+    let mut all_blueprints = Vec::<Blueprint>::new();
+    for line in lines_vec {
+        all_blueprints.push(Blueprint::new_from_line(&line));
+    }
+
+    // For each blueprint calculating the maximum efficiency
+    for blueprint in all_blueprints {
+
+    }
+
 
     result_part_1 = 0;
     result_part_2 = 0;
