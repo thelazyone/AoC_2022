23cb2e490f4ec63c9690d00b8650a4414c6b4733
thelazyone <giacomo.pantalone@gmail.com>
2023-05-23 20:31:08 +0200

    Set up Day 21

---
+++ exercise_21/src/main.rs
@@ -0,0 +1,64 @@
+// Exercise 21: decypher a set of unknown variables in a very long linear system
+
+// For reading/parsing
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+// utility
+
+// Primary Function
+fn execute (input_path : String)  -> Option<(u32, u32)> {
+
+    // Handling the reading/parsing
+    let file = File::open(input_path).unwrap();
+    let reader = BufReader::new(file);
+
+    // Results variables:
+    let result_part_1 : u32;
+    let result_part_2 : u32;
+
+    // First reading the input string - easy.
+    let mut lines_vec = Vec::<String>::new();
+    // Finally reading the stuff.
+    for curr_line in reader.lines() {
+        if let Ok(line) = curr_line {
+            lines_vec.push(line);
+        }
+    }
+    println!("read {} lines from input", lines_vec.len());
+    assert!(lines_vec.len() > 1);
+
+    result_part_1 = 0;
+    result_part_2 = 0;
+    Some((result_part_1, result_part_2))
+}
+
+// Main 
+fn main() -> io::Result<()> {
+    println!("Welcome to Advent of Code 2022 - Day XXX!");
+
+    let results = execute("./data/input.txt".to_string()).unwrap();
+    
+    println!("Part 1 result is {}.", results.0);
+    println!("Part 2 result is {}.", results.1);
+
+    // End of main
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // General Test
+    #[test]
+    fn global_test_part_1() {
+        assert_eq!(execute("./data/test.txt".to_string()).unwrap().0, 21);
+    }    
+
+    #[test]
+    fn global_test_part_2() {
+        assert_eq!(execute("./data/test.txt".to_string()).unwrap().1, 8);
+    }    
+}
