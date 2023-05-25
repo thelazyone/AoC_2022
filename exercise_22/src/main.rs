20d76135302759779e87963350475460efb599ff
thelazyone <giacomo.pantalone@gmail.com>
2023-05-25 20:51:08 +0200

    Solved Day 22 Part 1

--- exercise_22/src/main.rs
+++ exercise_22/src/main.rs
@@ -3,8 +3,218 @@
 // For reading/parsing
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+use regex;
 
 // utility
+#[derive(Clone, Debug)]
+enum RotationDirection {
+    Right,
+    Left,
+}
+
+#[derive(Clone, Debug)]
+enum CursorDirection {
+    Right = 0,
+    Down = 1,
+    Left = 2,
+    Up = 3,
+}
+fn number_to_cursor_direction(number : u32) -> CursorDirection {
+    match number {
+        0=>CursorDirection::Right,
+        1=>CursorDirection::Down,
+        2=>CursorDirection::Left,
+        3=>CursorDirection::Up,
+        _=> panic!("only four directions exist!"),
+    }
+}
+
+#[derive(Clone)]
+enum MovementCommand {
+    Advance(usize),
+    Rotate(RotationDirection),
+}
+
+#[derive(PartialEq, Eq, Clone)]
+enum WarpedBlock {
+    Floor,
+    Wall, 
+    Skip
+}
+
+#[derive(Clone, Debug)]
+struct WorldCursor{
+    position : (usize, usize), // col, pos
+    direction : CursorDirection
+}
+
+
+
+struct WarpedMap {
+    world_map : Vec<Vec<WarpedBlock>>,
+    movement_commands : Vec<MovementCommand>,
+    cursor : Option<WorldCursor>
+}
+
+impl WarpedMap {
+    fn new() -> WarpedMap {
+        WarpedMap {
+            world_map : Vec::new(),
+            movement_commands : Vec::new(),
+            cursor : None,
+        }
+    }
+
+    fn _get_warped_block_char(block_type : &WarpedBlock) -> char {
+        match block_type {
+            WarpedBlock::Floor=>'.',
+            WarpedBlock::Wall =>'#',
+            WarpedBlock::Skip =>' ',
+        }
+    }
+
+    // Parsing one line of the map.
+    fn add_line(&mut self, input_line : &String) {
+        self.world_map.push(input_line.chars().map(|character| {
+            match character {
+                '.'=>WarpedBlock::Floor,
+                '#'=>WarpedBlock::Wall,
+                ' '=>WarpedBlock::Skip,
+                _ => panic!("Unexpected char!"),
+            }
+        }).collect());
+
+        // If it's the first line, positioning the cursor.0
+        if self.world_map.len() == 1 {
+            let start_position = self.world_map[0].iter().position(|elem| elem == &WarpedBlock::Floor).unwrap();
+            self.cursor = Some(WorldCursor { 
+                position: (start_position, 0),
+                direction: CursorDirection::Right});
+        }
+
+        // This is costly but it's the more general way to ensure that the matrix is well defined.
+        self.pad_lines();
+    }
+
+    fn pad_lines(&mut self) {
+
+        // Checking the max among the lines, and padding al the other lines.
+        let max_length = self.world_map.iter()
+            .max_by(|x, y| x.len().cmp(&y.len())).unwrap().len();
+
+        for mut line in &mut self.world_map {
+            if line.len() < max_length {
+                line.extend(vec![WarpedBlock::Skip; max_length - line.len()]);
+            }
+        } 
+    }
+
+    // Parsing the last line of commands.
+    fn set_movement_commands(&mut self, input_line : &String) {
+        let re = regex::Regex::new(r"[0-9]+|[RL]").unwrap();
+        self.movement_commands = re.find_iter(input_line).map(|mat| {
+            match mat.as_str() {
+                "R"=>MovementCommand::Rotate(RotationDirection::Right),
+                "L"=>MovementCommand::Rotate(RotationDirection::Left),
+                _ => {
+                    if let Ok(num) = mat.as_str().parse::<i32>() {
+                        MovementCommand::Advance(num as usize)
+                    } else {
+                        panic!("Unexpected command: {}", mat.as_str())
+                    }
+                },
+            }
+        }).collect();
+    }
+
+    fn get_block_at_position(&self, pos: &(usize, usize)) -> Option<WarpedBlock> {
+        if self.world_map.is_empty(){
+            return None;
+        }
+        if self.world_map[0].is_empty(){
+            return None;
+        }
+
+        Some(self.world_map
+            [(self.world_map.len() + pos.1) % self.world_map.len()]
+            [(self.world_map[0].len() + pos.0) % self.world_map[0].len()].clone())
+    }
+
+    fn get_next_cursor_position(&self, cursor: &WorldCursor) -> (usize, usize) {
+        let mut current_position = (cursor.position.0 as i32, cursor.position.1 as i32);
+        loop {
+            let mut new_position = match cursor.direction {
+                CursorDirection::Down=>(current_position.0, current_position.1 + 1),
+                CursorDirection::Up=>(current_position.0, current_position.1 - 1),
+                CursorDirection::Right=>(current_position.0 + 1, current_position.1),
+                CursorDirection::Left=>(current_position.0 - 1, current_position.1),
+            };  
+
+            // applying the module to both col and row
+            new_position = (
+                (self.world_map[0].len() as i32 + new_position.0) % self.world_map[0].len() as i32,
+                (self.world_map.len() as i32 + new_position.1) % self.world_map.len() as i32,);
+
+            let found_block = 
+                self.get_block_at_position(&(new_position.0 as usize, new_position.1 as usize)).unwrap();
+            if found_block != WarpedBlock::Skip{
+                return (new_position.0 as usize, new_position.1 as usize);
+            }
+
+            // If it's a "skip", moving forward until found a proper block.
+            current_position = new_position;
+        }
+    }
+
+    // Handling the cursor movements
+    fn move_cursor(&mut self, steps: usize) {
+        let mut temp_cursor = self.cursor.as_ref().unwrap().clone();
+        for _ in 0..steps {
+            // Checking the next block in position
+            let next_position = self.get_next_cursor_position(&temp_cursor);
+            temp_cursor.position = match self.get_block_at_position(&next_position) {
+                Some(WarpedBlock::Floor)=>{
+                    next_position
+                },
+                Some(WarpedBlock::Wall)=>temp_cursor.position,//do nothing
+                Some(WarpedBlock::Skip)=>panic!("Encountered a 'skip' block while moving!"),
+                None=> panic!("found no block!"),
+            }
+        }
+
+        self.cursor.as_mut().unwrap().direction = temp_cursor.direction;
+        self.cursor.as_mut().unwrap().position = temp_cursor.position;
+    }
+
+    fn rotate_cursor(&mut self, direction: RotationDirection) {
+        let current_direction = self.cursor.as_mut().unwrap().direction.clone() as u32;
+        self.cursor.as_mut().unwrap().direction = match direction {
+            RotationDirection::Right=>number_to_cursor_direction((current_direction + 1) % 4),
+            RotationDirection::Left=>number_to_cursor_direction((current_direction + 3) % 4),
+        }
+    }
+
+    fn apply_all_movements(&mut self) {
+        for command in self.movement_commands.clone() {
+            match command {
+                MovementCommand::Advance(value)=>self.move_cursor(value.clone()),
+                MovementCommand::Rotate(value)=>self.rotate_cursor(value.clone()),
+            }
+        }
+    }
+
+    // For Debug Only:
+    fn _display_map(&self) {
+        for line in self.world_map.iter() {
+            for block in line {
+                print!("{}", WarpedMap::_get_warped_block_char(block));
+            }
+            println!("");
+        }
+    }
+    
+}
+
 
 // Primary Function
 fn execute (input_path : String)  -> Option<(u32, u32)> {
@@ -28,7 +238,33 @@ fn execute (input_path : String)  -> Option<(u32, u32)> {
     println!("read {} lines from input", lines_vec.len());
     assert!(lines_vec.len() > 1);
 
-    result_part_1 = 0;
+    // Creating the warped map: 
+    println!("Starting Part 1...");
+    let mut world_map = WarpedMap::new();
+    for line in lines_vec {
+        if  line.len() > 0 {
+            if !line.starts_with(' ') && !line.starts_with('.') && !line.starts_with('#')  {
+                world_map.set_movement_commands(&line);
+            }
+            else {
+                world_map.add_line(&line);
+            }
+        }
+        else {
+            continue;
+        }
+    }
+
+    // For debug only:
+    //world_map._display_map();
+
+    println!("Applying movements...");
+    world_map.apply_all_movements();
+    let final_cursor = world_map.cursor.as_ref().unwrap().clone();
+    result_part_1 = ((final_cursor.position.1 + 1) * 1000 +
+         (final_cursor.position.0 + 1) * 4) as u32 + final_cursor.direction as u32;
+    
+
     result_part_2 = 0;
     Some((result_part_1, result_part_2))
 }
@@ -54,7 +290,7 @@ mod tests {
     // General Test
     #[test]
     fn global_test_part_1() {
-        assert_eq!(execute("./data/test.txt".to_string()).unwrap().0, 21);
+        assert_eq!(execute("./data/test.txt".to_string()).unwrap().0, 6032);
     }    
 
     #[test]
